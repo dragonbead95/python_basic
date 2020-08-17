4b1010e08972c9b9fa1309ca3641084598cfc904
dragonbead95 <dragonbead95@naver.com>
2020-08-17 15:32:15 +0900

    파이썬 흐름제어 문제

---
+++ section05-3-q.py
@@ -0,0 +1,84 @@
+# Section05-3
+# 파이썬 흐름제어(제어문)
+# 제어문 관련 퀴즈(정답은 영상)
+
+# 1 ~ 5 문제 if 구문 사용
+# 1. 아래 딕셔너리에서 '가을'에 해당하는 과일을 출력하세요.
+q1 = {"봄": "딸기", "여름": "토마토", "가을": "사과"}
+print(q1["가을"])
+
+# 2. 아래 딕셔너리에서 '사과'가 포함되었는지 확인하세요.
+q2 = {"봄": "딸기", "여름": "토마토", "가을": "사과"}
+print("사과" in q2.values())
+
+# 3. 다음 점수 구간에 맞게 학점을 출력하세요.
+# 81 ~ 100 : A학점
+# 61 ~ 80 :  B학점
+# 41 ~ 60 :  C학점
+# 21 ~ 40 :  D학점
+#  0 ~ 20 :  E학점
+score = 100
+if score > 80 and score <= 100:
+    print("A학점")
+elif score > 60 and score <= 80:
+    print("B학점")
+elif score > 40 and score <= 60:
+    print("C학점")
+elif score > 20 and score <= 40:
+    print("D학점")
+elif score >= 0 and score <= 20:
+    print("E학점")
+
+# 4. 다음 세 개의 숫자 중 가장 큰수를 출력하세요.(if문 사용) : 12, 6, 18
+a = 12
+b = 6
+c = 18
+if a > b:
+    if a > c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b > c:
+        print(b)
+    else:
+        print(c)
+
+# 5. 다음 주민등록 번호에서 7자리 숫자를 사용해서 남자, 여자를 판별하세요. (1,3 : 남자, 2,4 : 여자)
+pi = "840505-1523859"
+if pi[7] == "1" or pi[7] == "3":
+    print("남자")
+elif pi[7] == "2" or pi[7] == "4":
+    print("여자")
+
+# 6 ~ 10 반복문 사용(while 또는 for)
+
+# 6. 다음 리스트 중에서 '정' 글자를 제외하고 출력하세요.
+q3 = ["갑", "을", "병", "정"]
+for q in q3:
+    if q != "정":
+        print(q, end=" ")
+print()
+# 7. 1부터 100까지 자연수 중 '홀수'만 한 라인으로 출력 하세요.
+for i in range(1, 101, 2):
+    print(i, end=" ")
+
+# 8. 아래 리스트 항목 중에서 5글자 이상의 단어만 출력하세요.
+q4 = ["nice", "study", "python", "anaconda", "!"]
+for q in q4:
+    if len(q) >= 5:
+        print(q)
+
+# 9. 아래 리스트 항목 중에서 소문자만 출력하세요.
+q5 = ["A", "b", "c", "D", "e", "F", "G", "h"]
+for q in q5:
+    if q.islower():
+        print(q)
+
+# 10. 아래 리스트 항목 중에서 소문자는 대문자로 대문자는 소문자로 출력하세요.
+q6 = ["A", "b", "c", "D", "e", "F", "G", "h"]
+for q in q6:
+    if q.isupper():
+        print(q.lower())
+    elif q.islower():
+        print(q.upper())
